399e708ec2a8dbd5808d139089438c026f6fd4d2
pablomac <p.arjonilla.android@gmail.com>
2023-10-03 11:07:24 +0200

    Painting each pixel <3

--- src/main.c
+++ src/main.c
@@ -3,55 +3,56 @@
 
 #include "pd_api.h"
 
+PlaydateAPI* pd;
+
+LCDBitmap* screenBuffer;
+int rowbytes;
+uint8_t* screenBufferData;
+
 static int update(void* userdata);
 
-int eventHandler(PlaydateAPI* pd, PDSystemEvent event, uint32_t arg)
+#define SCREEN_WIDTH = 400
+#define SCREEN_HEIGHT = 240
+
+int eventHandler(PlaydateAPI* playdate, PDSystemEvent event, uint32_t arg)
 {
     (void)arg;
     
     if ( event == kEventInit )
     {
+        pd = playdate;
         pd->system->setUpdateCallback(update, pd);
+        screenBuffer = pd->graphics->newBitmap(400, 240, kColorWhite);
+        pd->graphics->getBitmapData(screenBuffer, NULL, NULL, &rowbytes, NULL, &screenBufferData);
     }
-    
     return 0;
 }
 
 
-static int update(void* userdata)
-{
-    PlaydateAPI* pd = userdata;
-    
-    LCDBitmap* bitmap = pd->graphics->newBitmap(400, 240, kColorWhite);
-    
-    int rowbytes;
-    uint8_t* data;
-    
-    pd->graphics->getBitmapData(bitmap, NULL, NULL, &rowbytes, NULL, &data);
-    
- 
-    for (int y = 0; y < 240; y+=2)
+static void updateBuffer(void){
+    for (int y = 0; y < 240; y+=1)
     {
-        for (int x = 0; x < 400; x+=2)
+        for (int x = 0; x < 400; x+=1)
         {
-            int byteIndex = rowbytes * y + x / 8;
-            int bitIndex = x % 8;
-            data[byteIndex] &= ~(1 << bitIndex);
+            if(y==x * 240 / 400){
+                int byteIndex = rowbytes * y + x / 8;
+                int bitIndex = 7 - x % 8;
+                screenBufferData[byteIndex] &= ~(1 << bitIndex);
+            }
         }
     }
+}
 
-    // Clear the screen
-    pd->graphics->clear(kColorWhite);
+//BLACK screenBufferData[byteIndex] &= ~(1 << bitIndex);
+//WHITE screenBufferData[byteIndex] |= (1 << bitIndex);
+static int update(void* userdata)
+{
+    //Check player location
     
-    // Create a checkered pattern bitmap
+    updateBuffer();
     
-    // Draw the checkered pattern bitmap on the screen
-    pd->graphics->drawBitmap(bitmap, 0, 0, 0);
-    
-    // Free the checkered pattern bitmap
-    pd->graphics->freeBitmap(bitmap);
+    pd->graphics->drawBitmap(screenBuffer, 0, 0, 0);
     
     return 1;
-    
 }
 
