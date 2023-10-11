42f2e983ef392bec587dea8e5aada19d90884790
pablomac <p.arjonilla.android@gmail.com>
2023-10-11 20:09:51 +0200

    Basic navigation

--- src/main.c
+++ src/main.c
@@ -12,6 +12,8 @@ PlaydateAPI* pd;
 typedef struct {
     uint32_t x;
     uint32_t y;
+    float angle;
+    int height;
     int drawDistance;
     float fov;
 } camera_t;
@@ -19,7 +21,9 @@ typedef struct {
 camera_t camera = {
     .x = 20000,
     .y = 20000,
-    .drawDistance = 800,
+    .angle = M_PI/3.5,
+    .height = 100,
+    .drawDistance = 1200,
     .fov = M_PI/3.5
 };
 
@@ -43,15 +47,13 @@ float mtx9c[4][4] = {
     {1.0/9, 5.0/9, 2.0/9, 6.0/9}
 };
 
-
-
 //Delta time
 unsigned int lastFrame;
 int deltaTimeMS;
 
-
 //Movement
-float speed = 0.1f;
+float speedMovement = 0.1f;
+float speedRotation = 0.001f;
 
 
 int eventHandler(PlaydateAPI* playdate, PDSystemEvent event, uint32_t arg)
@@ -75,37 +77,45 @@ static void moveCamera(void){
     pd->system->getButtonState(&current, NULL, NULL);
     
     //LOG
-    //pd->system->logToConsole("X:%d - Y:%d ------ MS:%d",positionX, positionY);
+    //pd->system->logToConsole("Angle:%f",camera.angle);
     
     if (current & kButtonUp) {
-        camera.y -= speed * deltaTimeMS;
+        camera.x += speedMovement * deltaTimeMS * cos(camera.angle);
+        camera.y -= speedMovement * deltaTimeMS * sin(camera.angle);
     } else if (current & kButtonDown) {
-        camera.y += speed * deltaTimeMS;
+        camera.x -= speedMovement * deltaTimeMS * cos(camera.angle);
+        camera.y += speedMovement * deltaTimeMS * sin(camera.angle);
+   
     }
     
     if (current & kButtonLeft) {
-        camera.x -= speed * deltaTimeMS;
+        camera.angle -= speedRotation * deltaTimeMS;
     } else if (current & kButtonRight) {
-        camera.x += speed * deltaTimeMS;
+        camera.angle += speedRotation * deltaTimeMS;
     }
     
 }
 
 
-static void projectView(uint32_t cameraX, uint32_t cameraY, int drawDistance, float fov){
+static void projectView(uint32_t cameraX, uint32_t cameraY, int cameraHeight, float cameraAngle, int drawDistance, float fov){
+    
+    float sinAngle = sin(cameraAngle);
+    float cosAngle = cos(cameraAngle);
     
     float distance = drawDistance * tanf(fov);
     
-    float plx = - distance;
-    float ply = distance;
+    float plx = cosAngle * distance + sinAngle * distance;
+    float ply = sinAngle * distance - cosAngle * distance;
     
-    float prx = distance;
-    float pry = distance;
+    float prx = cosAngle * distance - sinAngle * distance;;
+    float pry = sinAngle * distance + cosAngle * distance;
+;
     
     //pd->system->logToConsole("x=%f, y=%f, distance=%f, plx=%f, ply=%f, prx=%f, pry=%f", cameraX, cameraY, distance,  plx, ply, prx, pry);
     
     for (int i = 0; i < 400; i++)
     {
+        
         float deltaX = (plx + ((prx - plx) / 400 * i)) / drawDistance;
         float deltaY = (ply + ((pry - ply) / 400 * i)) / drawDistance;
         
@@ -116,15 +126,19 @@ static void projectView(uint32_t cameraX, uint32_t cameraY, int drawDistance, fl
         
         int maxHeight = 239;
         
-        for (int z = 50; z < drawDistance; z++)
+        for (int z = 50; z < drawDistance; z+=2)
         {
             rx += deltaX;
             ry -= deltaY;
             
-            float height = perlin2d(rx, ry, 0.02, 1);
-            float color = perlin2d(rx, ry, 0.03, 1);
+            float height = perlin2d(rx, ry, 0.03, 1);
+            float color = perlin2d(rx, ry, 0.01, 1);
             
-            float screenHeight = ((239 * height/ z) * 120) + 100 ;
+            if(height>0.6f){
+                color = 1.0f;
+            }
+            
+            float screenHeight = ((239 * height/ z) * 120) + cameraHeight ;
             
             if(screenHeight<0){
                 screenHeight = 0;
@@ -135,8 +149,6 @@ static void projectView(uint32_t cameraX, uint32_t cameraY, int drawDistance, fl
             //pd->system->logToConsole("Current height is %f, and screen height is %d", height , screenHeight);
             
             if(screenHeight<maxHeight){
-                
-                
         
                 for(int y = screenHeight; y < maxHeight; y++){
                     
@@ -155,14 +167,14 @@ static void projectView(uint32_t cameraX, uint32_t cameraY, int drawDistance, fl
                 }
                 
                 for(int y = (239 - screenHeight); y >= (239-maxHeight); y--){
-                    
+
                     if(y > 119){
                         continue;
                     }
-                    
+
                     int byteIndex = 52 * y + i / 8; //52 Rowbytes
                     int bitIndex = 7 - i % 8;
-                    
+
                     if(color>mtx17c[y%4][i%4]-0.1f){
                         screenBufferData[byteIndex] &= ~(1 << bitIndex);
                     }else{
@@ -179,6 +191,7 @@ static void projectView(uint32_t cameraX, uint32_t cameraY, int drawDistance, fl
 //            int byteIndex = 52 * y + i / 8;
 //            int bitIndex = 7 - i % 8;
 //            screenBufferData[byteIndex] |= (1 << bitIndex);
+//            screenBufferData[byteIndex] &= ~(1 << bitIndex);
 //        }
     }
 }
@@ -190,7 +203,7 @@ static int update(void* userdata){
     lastFrame = pd->system->getCurrentTimeMilliseconds();
     
     moveCamera();
-    projectView(camera.x,camera.y, camera.drawDistance, camera.fov);
+    projectView(camera.x,camera.y, camera.height, camera.angle, camera.drawDistance, camera.fov);
     
     pd->graphics->drawBitmap(screenBuffer, 0, 0, 0);
     pd->system->drawFPS(10,10);
